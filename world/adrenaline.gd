3fc013ec42e12649bdca27b17eb2e5860c30e2e5
Beforg <bruno.capsqn@gmail.com>
2026-05-16 17:13:07 -0300

    feature: adicionando nó de adrenalina

---
+++ world/adrenaline.gd
@@ -0,0 +1,24 @@
+extends Area3D
+
+@onready var visual = $Sprite3D
+@onready var audio = $CollectSound
+@onready var collision = $CollisionShape3D
+
+func _on_body_entered(body: Node3D) -> void:
+	# Verifica se quem esbarrou foi o Player
+	if body.name == "Player":
+		
+		# 1. Avisa o Cérebro (GameManager)
+		GameManager.collect_adrenaline()
+		print("Pegou Adrenalina!: ", GameManager.adrenaline_count)
+		
+		# 2. Toca o som espacial de coleta
+		audio.play()
+		
+		# 3. Fica invisível e intocável imediatamente para não ser pego duas vezes
+		visual.visible = false
+		collision.set_deferred("disabled", true)
+		
+		# 4. A MÁGICA: Espera o som terminar antes de se autodestruir!
+		await audio.finished
+		queue_free()
